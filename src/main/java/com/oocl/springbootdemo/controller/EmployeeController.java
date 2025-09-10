6e0117f8f5d5fac30178018b757c9ad3b52f8301
KelvinTang <tangke3@oocl.com>
2025-09-10 17:44:59 +0800

    refactor: remove unused code

--- src/main/java/com/oocl/springbootdemo/controller/EmployeeController.java
+++ src/main/java/com/oocl/springbootdemo/controller/EmployeeController.java
@@ -1,9 +1,5 @@
 package com.oocl.springbootdemo.controller;
 
-import com.oocl.springbootdemo.EmployeeNotActiveException;
-import com.oocl.springbootdemo.EmployeeNotAmongLegalAgeException;
-import com.oocl.springbootdemo.EmployeeNotFoundException;
-import com.oocl.springbootdemo.EmployeeNotHavingAcceptablePaidException;
 import com.oocl.springbootdemo.object.Employee;
 import com.oocl.springbootdemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,25 +18,11 @@ public class EmployeeController {
 
     @PostMapping("")
     public ResponseEntity<Employee> createEmployees(@RequestBody Employee employee) {
-//        try {
-//            Employee result = employeeService.create(employee);
-//            return ResponseEntity.status(HttpStatus.CREATED).body(result);
-//        } catch (EmployeeNotAmongLegalAgeException | EmployeeNotHavingAcceptablePaidException e) {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-//        }
-
         return ResponseEntity.status(HttpStatus.CREATED).body(employeeService.create(employee));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Employee> getEmployee(@PathVariable long id)  {
-//        try {
-//            Employee result = employeeService.get(id);
-//            return ResponseEntity.status(HttpStatus.OK).body(result);
-//        } catch (EmployeeNotFoundException e) {
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-//        }
-
         return ResponseEntity.status(HttpStatus.OK).body(employeeService.get(id));
     }
 
@@ -56,27 +38,11 @@ public class EmployeeController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Employee> updateEmployee(@PathVariable long id, @RequestBody Employee updateEmployee) {
-//        try {
-//            Employee result = employeeService.update(id, updateEmployee);
-//            return ResponseEntity.status(HttpStatus.ACCEPTED).body(result);
-//        } catch (EmployeeNotFoundException e) {
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-//        } catch (EmployeeNotActiveException e) {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-//        }
-
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(employeeService.update(id, updateEmployee));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Employee> deleteEmployees(@PathVariable long id) {
-//        try {
-//            Employee result = employeeService.delete(id);
-//            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(result);
-//        } catch (EmployeeNotFoundException e) {
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-//        }
-
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(employeeService.delete(id));
     }
 }
