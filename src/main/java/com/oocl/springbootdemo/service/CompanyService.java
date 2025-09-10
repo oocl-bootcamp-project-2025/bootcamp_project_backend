34bd8d59ca9cea8194c0e6af7e2e97dd7811a37a
KelvinTang <tangke3@oocl.com>
2025-09-10 18:34:24 +0800

    feat: add CompanyService

--- src/main/java/com/oocl/springbootdemo/service/CompanyService.java
+++ src/main/java/com/oocl/springbootdemo/service/CompanyService.java
@@ -1,12 +1,12 @@
 package com.oocl.springbootdemo.service;
 
+import com.oocl.springbootdemo.exception.*;
 import com.oocl.springbootdemo.object.Company;
 import com.oocl.springbootdemo.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class CompanyService {
@@ -14,28 +14,29 @@ public class CompanyService {
     @Autowired
     private CompanyRepository companyRepository;
 
-    public Map<String, Long> create(Company company) {
-        companyRepository.save(company);
-        return Map.of("id", company.getId());
-    }
-
-    public Company update(long id, Company updateCompany) {
-        Company foundcompany = get(id);
-        if (foundcompany != null) {
-            return companyRepository.update(foundcompany, updateCompany);
-        }
-        return null;
+    public Company create(Company company) {
+        return companyRepository.save(company);
     }
 
     public Company get(long id) {
-        return companyRepository.findById(id);
+        Company foundCompany = companyRepository.findById(id);
+        if (foundCompany == null) {
+            throw new CompanyNotFoundException();
+        }
+        return foundCompany;
     }
 
     public List<Company> query(Integer page, Integer size) {
         return companyRepository.query(page, size);
     }
 
-    public void delete(long id) {
-        companyRepository.delete(id);
+    public Company update(long id, Company updateCompany) {
+        Company foundCompany = get(id);
+        return companyRepository.update(foundCompany, updateCompany);
+    }
+
+    public Company delete(long id) {
+        Company foundCompany = get(id);
+        return companyRepository.delete(foundCompany);
     }
 }
