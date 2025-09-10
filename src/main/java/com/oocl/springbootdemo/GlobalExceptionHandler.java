e5848b59e49013136f9378a6cf05eaab07540521
KelvinTang <tangke3@oocl.com>
2025-09-10 17:03:08 +0800

    feat: add exceptionHandler

---
+++ src/main/java/com/oocl/springbootdemo/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.oocl.springbootdemo;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleEmployeeNotFoundException(Exception e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(EmployeeNotActiveException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleEmployeeNotActiveException(Exception e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(EmployeeNotAmongLegalAgeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleEmployeeNotAmongLegalAgeException(Exception e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(EmployeeNotHavingAcceptablePaidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleEmployeeNotHavingAcceptablePaidException(Exception e) {
+        return e.getMessage();
+    }
+}
