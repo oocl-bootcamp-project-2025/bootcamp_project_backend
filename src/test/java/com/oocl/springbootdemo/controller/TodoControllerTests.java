76f8f8759a8cbb4cda14f4df52cfa54465d0490a
KelvinTang <tangke3@oocl.com>
2025-09-16 12:54:32 +0800

    test: add should_create_todo_when_post_given_a_valid_body

--- src/test/java/com/oocl/springbootdemo/controller/TodoControllerTests.java
+++ src/test/java/com/oocl/springbootdemo/controller/TodoControllerTests.java
@@ -1,15 +1,17 @@
 package com.oocl.springbootdemo.controller;
 
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oocl.springbootdemo.object.Company;
+import com.oocl.springbootdemo.object.Todo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,11 +21,32 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class TodoControllerTests {
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    Todo createTodo(String text) throws Exception {
+        Todo todo = new Todo();
+        todo.setText(text);
+        todo.setDone(false);
+        return todo;
+    }
 
     @Test
     void should_get_empty_json_when_get_storage_contains_no_todos() throws Exception {
         mockMvc.perform(get("/todos"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.length()").value(0));
+    }
+
+    @Test
+    void should_create_todo_when_post_given_a_valid_body() throws Exception {
+        Todo todo = createTodo("todo1");
+
+        mockMvc.perform(post("/todos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(todo)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.text").value("todo1"))
+                .andExpect(jsonPath("$.done").value(false));
     }
 }
