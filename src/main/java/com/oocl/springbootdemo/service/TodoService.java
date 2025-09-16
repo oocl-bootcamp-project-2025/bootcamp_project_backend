39520f40242d6123e9bdb8c39e74cd4c715d9b34
KelvinTang <tangke3@oocl.com>
2025-09-16 11:34:14 +0800

    feat: add TodoService

---
+++ src/main/java/com/oocl/springbootdemo/service/TodoService.java
@@ -0,0 +1,44 @@
+package com.oocl.springbootdemo.service;
+
+import com.oocl.springbootdemo.object.Company;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+//    public Company create(Company company) {
+//        return companyRepository.save(company);
+//    }
+
+//    public Company get(long id) {
+//        Company foundCompany = companyRepository.findById(id);
+//        if (foundCompany == null) {
+//            throw new CompanyNotFoundException();
+//        }
+//        return foundCompany;
+//    }
+
+    public List<Company> query() {
+        return todoRepository.query();
+    }
+
+//    public Company update(long id, UpdateCompanyRequest updateCompanyRequest) {
+//        Company foundCompany = get(id);
+//        return companyRepository.update(foundCompany, updateCompanyRequest);
+//    }
+//
+//    public void delete(long id) {
+//        Company foundCompany = get(id);
+//        companyRepository.delete(foundCompany);
+//    }
+//
+//    public void clear() {
+//        companyRepository.clearAll();
+//    }
+}
