2731efe09c0a9075071cac8b32853789b6c994ee
KelvinTang <tangke3@oocl.com>
2025-09-16 11:24:47 +0800

    test: add should_get_empty_json_when_get_storage_contains_no_todos

---
+++ src/test/java/com/oocl/springbootdemo/controller/TodoControllerTests.java
@@ -0,0 +1,29 @@
+package com.oocl.springbootdemo.controller;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TodoControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void should_get_empty_json_when_get_storage_contains_no_todos() throws Exception {
+        mockMvc.perform(get("/todos/"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.todos").value(new ArrayList<>()));
+    }
+}
