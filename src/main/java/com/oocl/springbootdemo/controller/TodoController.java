8f88b874d141058cb808e1187bb96f97e0a57f89
KelvinTang <tangke3@oocl.com>
2025-09-16 11:32:36 +0800

    feat: add Todo Controller

---
+++ src/main/java/com/oocl/springbootdemo/controller/TodoController.java
@@ -0,0 +1,48 @@
+package com.oocl.springbootdemo.controller;
+
+import com.oocl.springbootdemo.object.Company;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/companies")
+public class TodoController {
+
+    @Autowired
+    private TodoService todoService;
+
+    @GetMapping("")
+    public ResponseEntity<List<Company>> queryCompanies(
+            @RequestParam(required = false) Integer page,
+            @RequestParam(required = false) Integer size
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(todoService.query());
+    }
+
+
+//    @PostMapping("")
+//    public ResponseEntity<Company> createCompanies(@RequestBody Company company) {
+//        return ResponseEntity.status(HttpStatus.CREATED).body(companyService.create(company));
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Company> getCompany(@PathVariable long id) {
+//        return ResponseEntity.status(HttpStatus.OK).body(companyService.get(id));
+//    }
+//    @PutMapping("/{id}")
+//    public ResponseEntity<Company> updateCompanies(@PathVariable long id, @RequestBody UpdateCompanyRequest updateCompanyRequest) {
+//        return ResponseEntity.status(HttpStatus.ACCEPTED).body(companyService.update(id, updateCompanyRequest));
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<Company> deleteCompanies(@PathVariable long id) {
+//        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+//    }
+}
