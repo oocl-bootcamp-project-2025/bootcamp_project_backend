9f08fec51fd633c09d8fbcbdd591a02bb3d4fa49
KelvinTang <tangke3@oocl.com>
2025-09-16 11:44:36 +0800

    feat: add Todo

---
+++ src/main/java/com/oocl/springbootdemo/object/Todo.java
@@ -0,0 +1,21 @@
+package com.oocl.springbootdemo.object;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String text;
+    private boolean done;
+}
