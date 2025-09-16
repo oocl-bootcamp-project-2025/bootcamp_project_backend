90f0c35d098025b2556834e801e9e72294b4e0f9
KelvinTang <tangke3@oocl.com>
2025-09-16 11:44:01 +0800

    feat: update TodoController

--- src/main/java/com/oocl/springbootdemo/controller/TodoController.java
+++ src/main/java/com/oocl/springbootdemo/controller/TodoController.java
@@ -1,13 +1,12 @@
 package com.oocl.springbootdemo.controller;
 
-import com.oocl.springbootdemo.object.Company;
+import com.oocl.springbootdemo.object.Todo;
 import com.oocl.springbootdemo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,7 +19,7 @@ public class TodoController {
     private TodoService todoService;
 
     @GetMapping("")
-    public ResponseEntity<List<Company>> queryTodos() {
+    public ResponseEntity<List<Todo>> queryTodos() {
         return ResponseEntity.status(HttpStatus.OK).body(todoService.query());
     }
 
