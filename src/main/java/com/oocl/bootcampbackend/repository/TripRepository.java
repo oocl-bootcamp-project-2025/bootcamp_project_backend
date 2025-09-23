29872c8af248acc346e81f838be8ceac7e0a7460
vegafeng <vega.feng@oocl.com>
2025-09-23 18:38:53 +0800

    feat(#29): add  repository method for user and trip

--- src/main/java/com/oocl/bootcampbackend/repository/TripRepository.java
+++ src/main/java/com/oocl/bootcampbackend/repository/TripRepository.java
@@ -1,4 +1,14 @@
 package com.oocl.bootcampbackend.repository;
 
+import com.oocl.bootcampbackend.entity.Trip;
+import com.oocl.bootcampbackend.entity.User;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Repository
 public interface TripRepository {
+    public List<Trip> findAll();
+    public List<Trip> findByUser(User user);
 }
--- src/main/java/com/oocl/bootcampbackend/repository/UserRepository.java
+++ src/main/java/com/oocl/bootcampbackend/repository/UserRepository.java
@@ -1,4 +1,15 @@
 package com.oocl.bootcampbackend.repository;
 
+import com.oocl.bootcampbackend.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public interface UserRepository {
+    public User findByPhone(String phone);
+    public List<User> findAll();
+    public User findById(Long id);
+    public void save(User user);
 }
