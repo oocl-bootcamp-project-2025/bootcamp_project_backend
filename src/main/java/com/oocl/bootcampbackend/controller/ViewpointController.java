acd14cb3119ac2ded76b5b9e21c2916d270517cb
Cyrologistic <iclemo0.0@gmail.com>
2025-09-23 22:52:49 +0800

    fix: db schema for attraction

--- src/main/java/com/oocl/bootcampbackend/controller/ViewpointController.java
+++ src/main/java/com/oocl/bootcampbackend/controller/ViewpointController.java
@@ -4,17 +4,13 @@ import com.oocl.bootcampbackend.service.ViewpointService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/viewpoints")
 public class ViewpointController {
-
     @Autowired
     private ViewpointService viewpointService;
 
@@ -27,4 +23,5 @@ public class ViewpointController {
     public ResponseEntity<List<String>> getAreas() {
         return ResponseEntity.status(HttpStatus.OK).body(viewpointService.getAllArea());
     }
+
 }
