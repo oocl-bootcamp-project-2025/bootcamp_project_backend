f4e6af08116a5798afcd6f714e40969495e9adbc
vegafeng <vega.feng@oocl.com>
2025-09-23 18:19:52 +0800

    feat(#29): add trip service

---
+++ src/main/java/com/oocl/bootcampbackend/service/TripService.java
@@ -0,0 +1,10 @@
+package com.oocl.bootcampbackend.service;
+
+import com.oocl.bootcampbackend.controller.dto.TripDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TripService {
+    public void save(TripDTO tripDTO) {
+    }
+}
