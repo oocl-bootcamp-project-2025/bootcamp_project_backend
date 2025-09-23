d41ac6b87f82aff70d1e053f0eb70b1733b62d32
vegafeng <vega.feng@oocl.com>
2025-09-23 16:34:18 +0800

    feat(#29): add trip field

--- src/main/java/com/oocl/bootcampbackend/entity/Trip.java
+++ src/main/java/com/oocl/bootcampbackend/entity/Trip.java
@@ -9,6 +9,13 @@ public class Trip {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String description;
+    private String duration;
+    private String time;
+    private String location;
+    private String images;
+    private String experts;
+    private String start;
 
 
     // 3. 核心：关联User表的外键配置
@@ -34,5 +41,69 @@ public class Trip {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getImages() {
+        return images;
+    }
+
+    public void setImages(String images) {
+        this.images = images;
+    }
+
+    public String getExperts() {
+        return experts;
+    }
+
+    public void setExperts(String experts) {
+        this.experts = experts;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
     }
 }
