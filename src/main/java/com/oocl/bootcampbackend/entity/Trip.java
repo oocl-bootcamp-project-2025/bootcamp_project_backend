13654c73e8347493b16c93b4a5a3c9f54de9c943
vegafeng <vega.feng@oocl.com>
2025-09-23 20:40:13 +0800

    feat(#29): add field for trip to get data from front

--- src/main/java/com/oocl/bootcampbackend/entity/Trip.java
+++ src/main/java/com/oocl/bootcampbackend/entity/Trip.java
@@ -24,6 +24,7 @@ public class Trip {
     private String images;
     private String experts;
     private String start;
+    private String day;
     private Boolean isDone;
 
 
@@ -36,4 +37,99 @@ public class Trip {
     )
     private User user; // 关联的User对象（而非直接用userId！JPA通过这个对象维护关联关系，userId会自动映射为数据库字段）
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getImages() {
+        return images;
+    }
+
+    public void setImages(String images) {
+        this.images = images;
+    }
+
+    public String getExperts() {
+        return experts;
+    }
+
+    public void setExperts(String experts) {
+        this.experts = experts;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public Boolean getDone() {
+        return isDone;
+    }
+
+    public void setDone(Boolean done) {
+        isDone = done;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
