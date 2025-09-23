deafc203d0a902da396ed689c09fbe4d2473b161
vegafeng <vega.feng@oocl.com>
2025-09-23 18:20:06 +0800

    feat(#29): add user service

---
+++ src/main/java/com/oocl/bootcampbackend/service/UserService.java
@@ -0,0 +1,22 @@
+package com.oocl.bootcampbackend.service;
+
+import com.oocl.bootcampbackend.controller.dto.UserDTO;
+import com.oocl.bootcampbackend.entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public void save(User user) {
+    }
+
+    public void validateUser(User user) {
+    }
+
+    public void login(UserDTO userDTO) {
+
+    }
+
+    public void register(UserDTO userDTO) {
+
+    }
+}
