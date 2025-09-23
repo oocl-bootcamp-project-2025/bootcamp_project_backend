7e4fec5790f3e6e14f281cd0a1fc194235583a19
KelvinTang <tangke3@oocl.com>
2025-09-23 09:35:04 +0800

    feat: add application-prod

--- src/main/resources/db/migration/V20250911200000__Create_table_company.sql
+++
@@ -1,4 +0,0 @@
-create table if not exists company (
-   id BIGINT auto_increment primary key,
-   name varchar(255) null
-);
--- src/main/resources/db/migration/todo/V20250916114500__create_table_todo.sql
+++
@@ -1,5 +0,0 @@
-create table if not exists todo (
-    id BIGINT auto_increment primary key,
-    text varchar(255) null,
-    done bit(1) null
-);
