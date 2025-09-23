cd52d437207cc046ab434d31511207bc5aaec65a
KelvinTang <tangke3@oocl.com>
2025-09-23 20:04:15 +0800

    feat: add account

---
+++ src/main/java/com/oocl/bootcampbackend/entity/Account.java
@@ -0,0 +1,37 @@
+package com.oocl.bootcampbackend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String phone;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
