49de89b6097485422434d89aee0f6aba74b579aa
vegafeng <vega.feng@oocl.com>
2025-09-23 20:58:28 +0800

    refactor(#29): add exception handler for null phone

---
+++ src/main/java/com/oocl/bootcampbackend/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.oocl.bootcampbackend.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException() {
+        super();
+    }
+}
+
--- src/main/java/com/oocl/bootcampbackend/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/oocl/bootcampbackend/exception/handler/GlobalExceptionHandler.java
@@ -1,8 +1,17 @@
 package com.oocl.bootcampbackend.exception.handler;
 
 
+import com.oocl.bootcampbackend.exception.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleBadRequestException(BadRequestException e) {
+        return e.getMessage();
+    }
 }
