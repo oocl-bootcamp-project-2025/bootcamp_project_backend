77c627bc31d4b23b679bc9599f088602f1f24757
Cyrologistic <iclemo0.0@gmail.com>
2025-09-23 23:50:44 +0800

    feat: implemented business logic and repository for attractions

--- src/main/java/com/oocl/bootcampbackend/repository/ViewpointRepository.java
+++ src/main/java/com/oocl/bootcampbackend/repository/ViewpointRepository.java
@@ -8,4 +8,5 @@ public interface ViewpointRepository {
     Viewpoint save(Viewpoint viewpoint);
     List<String> findArea(String area);
     List<String> findAllArea();
+    List<Viewpoint> findViewPointsByPreference(int[] preference);
 }
