f999746c3900a5e8fe52eee67bc5a0987f406fd8
Cyrologistic <iclemo0.0@gmail.com>
2025-09-23 21:51:28 +0800

    test: test cicd

--- src/main/java/com/oocl/bootcampbackend/repository/dao/AccountJpaRepository.java
+++ src/main/java/com/oocl/bootcampbackend/repository/dao/AccountJpaRepository.java
@@ -1,6 +1,5 @@
 package com.oocl.bootcampbackend.repository.dao;
 
-import com.oocl.bootcampbackend.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountJpaRepository extends JpaRepository<Account, Long> {
