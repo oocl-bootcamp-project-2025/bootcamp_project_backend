059da383c87c86a2ccb7654ecc0a7d1e8720fd0f
vegafeng <vega.feng@oocl.com>
2025-09-23 20:42:09 +0800

    feat(#29): change TripController to convert data from front and handle it

--- src/main/java/com/oocl/bootcampbackend/controller/TripController.java
+++ src/main/java/com/oocl/bootcampbackend/controller/TripController.java
@@ -1,6 +1,8 @@
 package com.oocl.bootcampbackend.controller;
 
+import com.oocl.bootcampbackend.controller.dto.ItineraryRequest;
 import com.oocl.bootcampbackend.controller.dto.TripDTO;
+import com.oocl.bootcampbackend.controller.dto.TripsDTO;
 import com.oocl.bootcampbackend.entity.Trip;
 import com.oocl.bootcampbackend.service.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +19,11 @@ public class TripController
     private TripService tripService;
 
     @PostMapping("/trips")
-    public ResponseEntity<Void> saveTrips(@RequestBody TripDTO tripDTO, @RequestBody String phone){
+    public ResponseEntity<Void> saveTrips(@RequestBody ItineraryRequest itineraryRequest){
         // 1. 转化成Trip列表
-
+//        TripsDTO tripDTOs = itineraryRequest.getTrips();
         // 2. 存进数据库
-        tripService.save(tripDTO, phone);
+        tripService.save(itineraryRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
