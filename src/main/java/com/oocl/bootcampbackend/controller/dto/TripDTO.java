2915cab3502b81b4457da987a77a4b03726aaa44
vegafeng <vega.feng@oocl.com>
2025-09-23 20:40:25 +0800

    feat(#29): add field for tripDTO to get data from front

--- src/main/java/com/oocl/bootcampbackend/controller/dto/TripDTO.java
+++ src/main/java/com/oocl/bootcampbackend/controller/dto/TripDTO.java
@@ -3,8 +3,7 @@ package com.oocl.bootcampbackend.controller.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+
 public class TripDTO {
     private String name;
     private String description;
@@ -14,4 +13,77 @@ public class TripDTO {
     private String images;
     private String experts;
     private String start;
+    private String day;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getImages() {
+        return images;
+    }
+
+    public void setImages(String images) {
+        this.images = images;
+    }
+
+    public String getExperts() {
+        return experts;
+    }
+
+    public void setExperts(String experts) {
+        this.experts = experts;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
 }
