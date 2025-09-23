404679ecf4596f1aac165d641403c548fb1a77a7
Cyrologistic <iclemo0.0@gmail.com>
2025-09-23 21:51:28 +0800

    test: test cicd

--- src/main/java/com/oocl/bootcampbackend/repository/ExpertRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oocl.bootcampbackend.repository;
-
-import com.oocl.bootcampbackend.entity.Viewpoint;
-
-import java.util.List;
-
-public interface ExpertRepository {
-
-}
--- src/main/java/com/oocl/bootcampbackend/repository/dao/ExpertJpaRepository.java
+++ src/main/java/com/oocl/bootcampbackend/repository/dao/ExpertJpaRepository.java
@@ -1,10 +1,6 @@
 package com.oocl.bootcampbackend.repository.dao;
 
-import com.oocl.bootcampbackend.entity.Expert;
-import com.oocl.bootcampbackend.entity.Viewpoint;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface ExpertJpaRepository extends JpaRepository<Expert, Long> {
 
