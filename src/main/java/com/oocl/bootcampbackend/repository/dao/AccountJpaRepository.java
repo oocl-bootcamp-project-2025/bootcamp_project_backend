38b9096b9439ab2d2dfa92859738203bd1692d0b
Cyrologistic <iclemo0.0@gmail.com>
2025-09-23 21:53:36 +0800

    test: test cicd

--- src/main/java/com/oocl/bootcampbackend/repository/dao/AccountJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.oocl.bootcampbackend.repository.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountJpaRepository extends JpaRepository<Account, Long> {
-    Account findByPhone(String phone);
-}
--- src/main/java/com/oocl/bootcampbackend/repository/dao/ExpertJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.oocl.bootcampbackend.repository.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpertJpaRepository extends JpaRepository<Expert, Long> {
-
-}
