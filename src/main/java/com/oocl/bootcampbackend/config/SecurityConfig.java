d54a1e473da71962cdee4c78902936e837e0340e
vegafeng <vega.feng@oocl.com>
2025-09-24 10:06:05 +0800

    feat(#32): move token filter before auth login filter

--- src/main/java/com/oocl/bootcampbackend/config/SecurityConfig.java
+++ src/main/java/com/oocl/bootcampbackend/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,14 +25,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
+            .addFilterBefore(new TokenAuthFilter(), UsernamePasswordAuthenticationFilter.class)
         .authorizeHttpRequests(auth -> auth
             .requestMatchers(HttpMethod.GET, "/trips").authenticated()
             .anyRequest().permitAll()
         )
-        .addFilterBefore(new TokenAuthFilter(), UsernamePasswordAuthenticationFilter.class)
-        .formLogin(form -> form.disable())
-        .httpBasic(httpBasic -> httpBasic.disable())
-        .csrf(csrf -> csrf.disable())
+        .formLogin(AbstractHttpConfigurer::disable)
+        .httpBasic(AbstractHttpConfigurer::disable)
+        .csrf(AbstractHttpConfigurer::disable)
         .sessionManagement(session -> session
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
